58db769e3a8f0efd0358c03aa09cd320e15e6f19
Jordan Husney <jordan.husney@gmail.com>
2020-08-04 14:45:26 -0700

    add buildpacks for dokku, add userId tag
    Oddly I thought I wanted to use the Dockerfile version for our deployment,
    after playing around with using a sidecar container the path of least
    resistance ended up being to use the Heroku buildpacks and just run
    a fresh instance of DogStatsD within the built container.
    
    After piping some data into Datadog, I felt the urge to be able to
    tag events by userId which will enable us to get a real count of
    unique active users in any window of time.

--- app.py
+++ app.py
@@ -1,23 +1,29 @@
 import hmac
 import hashlib
+import logging
 import os
 from flask import Flask, jsonify, request, abort
 from datadog import initialize, api, statsd
 
 # get keys from enfironment variables
 SEGMENT_SHARED_SECRET = os.environ['SEGMENT_SHARED_SECRET']
+DATADOG_API_KEY = os.environ['DD_API_KEY']
+DATADOG_APP_KEY = os.environ['DD_APP_KEY']
 
 # initialize datadog
 options = {
-    'statsd_host': os.getenv('DATADOG_STATSD_HOST', None)
+    'api_key': DATADOG_API_KEY,
+    'app_key': DATADOG_APP_KEY
 }
-
 initialize(**options)
 
 app = Flask(__name__)
+app.logger.setLevel(logging.INFO)
+app.logger.info('event: %r', options)
 
 @app.route('/')
 def index():
+    app.logger.info('viewed root route')
     return "Segment2Datadog is up and running!"
 
 @app.route('/api/<string:source>', methods=['POST'])
@@ -32,5 +38,18 @@ def segment2datadog(source):
     content = request.get_json(silent=True)
     # increment event counter in datadog
     if content['type'] == 'track':
-        statsd.increment('segment.event', tags = ['source:' + source, 'event:' + '-'.join(content['event'].split()), 'type:' + content['type']])
+        app.logger.info(
+          '{ event: "%s", userId: "%s" }',
+          '-'.join(content['event'].split()),
+          content['userId']
+        )
+        statsd.increment(
+          'segment.event',
+          tags = [
+            'source:' + source,
+            'event:' + '-'.join(content['event'].split()),
+            'type:' + content['type'],
+            'user-id:' + content['userId'].replace('|','_')
+          ]
+        )
     return jsonify({'source': source, 'data': content})
